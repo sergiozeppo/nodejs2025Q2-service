6f9adc44aab4aa7c07ff4de9b0b39c6b9f33e879
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2025-06-03 01:37:04 +0500

    feat: add openAPI to albums and artists

--- src/albums/albums.controller.ts
+++ src/albums/albums.controller.ts
@@ -12,34 +12,55 @@ import {
 } from '@nestjs/common';
 import { AlbumsService } from './albums.service';
 import { CreateAlbumDto } from './create-album.dto';
+import { ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
 
 @Controller('album')
 export class AlbumsController {
   constructor(private readonly albumsService: AlbumsService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get list of albums' })
+  @ApiResponse({ status: 200, description: 'List of albums' })
   getAll() {
     return this.albumsService.getAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get unique album by ID' })
+  @ApiParam({ name: 'id', description: 'Album UUID' })
+  @ApiResponse({ status: 200, description: 'Album found' })
+  @ApiResponse({ status: 404, description: 'Album not found' })
+  @ApiResponse({ status: 400, description: 'Invalid Album ID' })
   getById(@Param('id', new ParseUUIDPipe()) id: string) {
     return this.albumsService.getById(id);
   }
 
   @Post()
+  @ApiOperation({ summary: 'Create a new album' })
+  @ApiResponse({ status: 201, description: 'Album created successfully' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
   @HttpCode(HttpStatus.CREATED)
   create(@Body() createAlbum: CreateAlbumDto) {
     return this.albumsService.create(createAlbum);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete album' })
+  @ApiParam({ name: 'id', description: 'Album UUID' })
+  @ApiResponse({ status: 204, description: 'Album deleted successfully' })
+  @ApiResponse({ status: 404, description: 'Album not found' })
+  @ApiResponse({ status: 400, description: 'Invalid Album ID' })
   @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param('id', new ParseUUIDPipe()) id: string) {
     this.albumsService.delete(id);
   }
 
   @Put(':id')
+  @ApiOperation({ summary: 'Update album' })
+  @ApiParam({ name: 'id', description: 'Album UUID' })
+  @ApiResponse({ status: 200, description: 'Album updated successfully' })
+  @ApiResponse({ status: 404, description: 'Album not found' })
+  @ApiResponse({ status: 400, description: 'Invalid Album ID' })
   update(
     @Param('id', new ParseUUIDPipe()) id: string,
     @Body() updateAlbum: CreateAlbumDto,
--- src/artists/artists.controller.ts
+++ src/artists/artists.controller.ts
@@ -12,34 +12,55 @@ import {
 } from '@nestjs/common';
 import { ArtistsService } from './artists.service';
 import { CreateArtistDto } from './create-artist.dto';
+import { ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
 
 @Controller('artist')
 export class ArtistsController {
   constructor(private readonly artistsService: ArtistsService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get list of artists' })
+  @ApiResponse({ status: 200, description: 'List of artists' })
   getAll() {
     return this.artistsService.getAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get unique artist by ID' })
+  @ApiParam({ name: 'id', description: 'Artist UUID' })
+  @ApiResponse({ status: 200, description: 'Artist found' })
+  @ApiResponse({ status: 404, description: 'Artist not found' })
+  @ApiResponse({ status: 400, description: 'Invalid Artist ID' })
   getById(@Param('id', new ParseUUIDPipe()) id: string) {
     return this.artistsService.getById(id);
   }
 
   @Post()
+  @ApiOperation({ summary: 'Create a new artist' })
+  @ApiResponse({ status: 201, description: 'Artist created successfully' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
   @HttpCode(HttpStatus.CREATED)
   create(@Body() createArtist: CreateArtistDto) {
     return this.artistsService.create(createArtist);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete artist' })
+  @ApiParam({ name: 'id', description: 'Artist UUID' })
+  @ApiResponse({ status: 204, description: 'Artist deleted successfully' })
+  @ApiResponse({ status: 404, description: 'Artist not found' })
+  @ApiResponse({ status: 400, description: 'Invalid Artist ID' })
   @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param('id', new ParseUUIDPipe()) id: string) {
     this.artistsService.delete(id);
   }
 
   @Put(':id')
+  @ApiOperation({ summary: 'Update artist' })
+  @ApiParam({ name: 'id', description: 'Artist UUID' })
+  @ApiResponse({ status: 200, description: 'Artist updated successfully' })
+  @ApiResponse({ status: 404, description: 'Artist not found' })
+  @ApiResponse({ status: 400, description: 'Invalid Artist ID' })
   update(
     @Param('id', new ParseUUIDPipe()) id: string,
     @Body() updateArtist: CreateArtistDto,
