15b5a2762afca1fcec029cbb693a396c6316ee1f
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2025-06-03 01:06:28 +0500

    feat: add Swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,13 @@ import { AlbumsModule } from './albums/albums.module';
 import { ArtistsModule } from './artists/artists.module';
 import { TracksModule } from './tracks/tracks.module';
 import { FavoritesModule } from './favorites/favorites.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     UsersModule,
     AlbumsModule,
     ArtistsModule,
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 4000);
+  const configDoc = new DocumentBuilder()
+    .setTitle('Home Library Service')
+    .setDescription('API docs for Home Library Service')
+    .setVersion('v1.0')
+    .addBearerAuth()
+    .build();
+
+  const doc = SwaggerModule.createDocument(app, configDoc);
+  SwaggerModule.setup('doc', app, doc);
+
+  const configService = app.get(ConfigService);
+  const port = (configService.get('PORT') as number) || 4000;
+
+  await app.listen(port);
+  console.log(`Server is running on: http://localhost:${port}`);
 }
 bootstrap();
